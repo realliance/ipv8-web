750e27478e65950ae84b198ba5b206d730ae77a0
Christopher Miller <admin@mctherealm.net>
2022-07-28 19:30:07 -0700

    Fixed verb

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -11,7 +11,7 @@ const register = () => (
       <h1 className="text-3xl font-bold mb-4">Register Now!</h1>
       <EndpointBlock endpoint="login.nightly.ipv8.dev" title="Login Endpoint">
         <RouteCard verb={RouteVerb.POST} description="Registers a new unlicensed user" path="/register" />
-        <RouteCard verb={RouteVerb.GET} authorized description="Authenticates a user" path="/login" />
+        <RouteCard verb={RouteVerb.POST} authorized description="Authenticates a user" path="/login" />
         <RouteCard verb={RouteVerb.GET} authorized description="Retrieves information about a user" path="/user" />
       </EndpointBlock>
     </div>
